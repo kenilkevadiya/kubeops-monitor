7299127785769980510ddb51a159292fc0694f58
kenilkevadiya <kskevadiya2002@gmail.com>
2024-11-24 08:23:37 -0400

    Added eks.py

--- ecr.py
+++ ecr.py
@@ -2,7 +2,7 @@ import boto3
 
 ecr_client = boto3.client('ecr')
 
-repository_name = "kubeops-monitor-app"
+repository_name = "kubeops-monitoring_app_image"
 response = ecr_client.create_repository(repositoryName=repository_name)
 
 repository_uri = response ['repository']['repositoryUri']
